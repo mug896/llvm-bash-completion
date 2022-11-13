cdb655b2b80493b5550850b7583289d3276c6ecb
Hyunho Cho <mug896@gmail.com>
2022-11-14 01:12:28 +0900

    update

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# llvm-bash-completion
-This is a bash completion function for llvm commands 
+# LLVM Bash Completion
+
+This is a bash completion function for the following commands.
+these commands can be installed in Ubuntu 22.10 using
+
+> apt install llvm clang lld clang-format clangd 
+
+    llc lli opt bugpoint dsymutil obj2yaml yaml2obj sanstats verify-uselistorder 
+    llvm-addr2line llvm-ar llvm-as llvm-bcanalyzer llvm-c-test llvm-cat 
+    llvm-cfi-verify llvm-config llvm-cxxfilt llvm-dis llvm-dlltool llvm-dwarfdump
+    llvm-exegesis llvm-extract llvm-link llvm-lto llvm-lto2 llvm-mc llvm-mca
+    llvm-modextract llvm-nm llvm-objcopy llvm-objdump llvm-opt-report
+    llvm-ranlib llvm-readelf llvm-readobj llvm-rtdyld llvm-size llvm-split
+    llvm-stress llvm-strings llvm-strip llvm-symbolizer llvm-tblgen
+    llvm-cov llvm-pdbutil llvm-profdata llvm-undname llvm-xray 
+    clang-format clang-format-diff ld.lld clang-cpp clangd
+
+Completion function for `clang` command can be downloaded separately
+from this url https://github.com/mug896/clang-bash-completion
+
+
+## Usage
+
+You can search for completion words using `*`, `?`, `[...]` glob characters
+while writing command line like this
+
+```sh
+bash$ opt -*print*[tab]
+. . .
+--print-after=
+--print-after-all
+--print-after-isel
+--print-all-options
+--print-before=
+--print-before-all
+--print-before-changed
+. . .                       # "q"
+[tab]                       # [tab] to exit to the prompt.
+```
+
+
+## Installation
+
+Copy contents of gcc-bash-completion.sh to ~/.bash_completion  
+open new terminal and try auto completion !
+
+
+> please leave an issue above if you have any problems using this script.
---
+++ llvm-bash-completion.sh
@@ -0,0 +1,139 @@
+_init_comp_wordbreaks()
+{
+    if [[ $PROMPT_COMMAND == *";COMP_WORDBREAKS="* ]]; then
+        [[ $PROMPT_COMMAND =~ ^:\ ([^;]+)\; ]]
+        [[ ${BASH_REMATCH[1]} != "${COMP_WORDS[0]}" ]] && eval "${PROMPT_COMMAND%%$'\n'*}"
+    fi
+    if [[ $PROMPT_COMMAND != *";COMP_WORDBREAKS="* ]]; then
+        PROMPT_COMMAND=": ${COMP_WORDS[0]};COMP_WORDBREAKS=${COMP_WORDBREAKS@Q};\
+        "$'PROMPT_COMMAND=${PROMPT_COMMAND#*$\'\\n\'}\n'$PROMPT_COMMAND
+    fi
+}
+_llvm_bind() { bind '"\011": complete' ;}
+_llvm_header() 
+{
+    cur=${COMP_WORDS[COMP_CWORD]} cur_o=$cur
+    [[ ${COMP_LINE:COMP_POINT-1:1} = " " || $COMP_WORDBREAKS == *$cur* ]] && cur=""
+    prev=${COMP_WORDS[COMP_CWORD-1]} prev_o=$prev
+    [[ $prev == [,=] ]] && prev=${COMP_WORDS[COMP_CWORD-2]}
+    if (( COMP_CWORD > 4 )); then
+        [[ $cur_o == [,=] ]] && prev2=${COMP_WORDS[COMP_CWORD-3]} || prev2=${COMP_WORDS[COMP_CWORD-4]}
+    fi
+    comp_line2=${COMP_LINE:0:$COMP_POINT}
+    eval arr=( $comp_line2 ) 2> /dev/null
+    for (( i = ${#arr[@]} - 1; i > 0; i-- )); do
+        if [[ ${arr[i]} == -* ]]; then
+            preo=${arr[i]%%[^[:alnum:]_-]*}
+            [[ ($preo == ${comp_line2##*[ ]}) && ($preo == $cur_o) ]] && preo=""
+            break
+        fi
+    done
+}
+_llvm_footer()
+{
+    if [[ -z $COMPREPLY ]]; then
+        words=$( <<< $words sed -E 's/^[[:blank:]]+|[[:blank:]]+$//g' )
+        IFS=$'\n' COMPREPLY=($(compgen -W "$words" -- "$cur"))
+    fi
+    [[ ${COMPREPLY: -1} == "=" ]] && compopt -o nospace
+}
+_llvm_search()
+{
+    local -A aar; IFS=$'\n'; echo
+    words=$(<<< $help sed -En 's/^[ ]{,10}(--?[[:alnum:]][^ =,]*)(=?).*/\1\2/; tX; b; :X s/(.*)\[=/\1\n\1=/; p')
+    for v in $words; do 
+        let aar[$v]++
+        if [[ $v == $cur && ${aar[$v]} -eq 1 ]]; then
+            echo -e "\\e[36m$v\\e[0m"
+        fi
+    done | less -FRSXi
+    IFS=$'\n' COMPREPLY=( "${cur_o%%[[*?]*}" )
+    bind -x '"\011": _llvm_bind'
+}
+_llvm() 
+{
+    # It is recommended that all completion functions start with _init_comp_wordbreaks,
+    # regardless of whether you change the COMP_WORDBREAKS variable afterward.
+    _init_comp_wordbreaks
+    COMP_WORDBREAKS=${COMP_WORDBREAKS//:/}
+    [[ $COMP_WORDBREAKS != *","* ]] && COMP_WORDBREAKS+=","
+
+    local IFS=$' \t\n' cur cur_o prev prev_o prev2 preo
+    local cmd=$1 cmd2 words comp_line2 help args arr i v
+    _llvm_header
+
+    help=$({ $cmd --help-list-hidden || $cmd --help-hidden || $cmd --help-list || $cmd --help ;} 2>&1 )
+#     help+=$'\n'$( man $cmd 2>/dev/null | sed -En '/^[ ]*OPTIONS:?$/,/^[ ]*[A-Z ]+$/{
+#         /^\s{,10}-/{ s/ (-[[:alnum:]-]+=?)|./\1\n/g; s/\n+|-\n/\n/g; p }}')
+
+    if [[ $cur == -*[[*?]* ]]; then
+        _llvm_search
+
+    elif [[ $cur == -* ]]; then
+        if [[ $cmd == llvm-c-test ]]; then
+            words=$(llvm-c-test |& sed -E 's/ (-[[:alnum:]-]+)|./\1/g;')
+        else
+            words=$(<<< $help sed -En 's/^[ ]{,10}(--?[[:alnum:]][^ =,]*)(=?).*/\1\2/; tX; b; :X s/(.*)\[=/\1\n\1=/; p')
+        fi
+    
+    elif [[ $cmd == opt && $prev == --passes ]]; then
+        words=$(opt --print-passes | sed -E '/^[^ ].+:$/d')
+
+    elif [[ $prev == -* || "," == @($cur_o|$prev_o) ]]; then
+        [[ $prev == -* ]] && args=$prev || args=$preo
+        words=$(<<< $help sed -En '/'"$args"'/{ :X n; s/^[ ]{,10}=([^ ]+).*/\1/p; tX}')
+    fi
+
+    _llvm_footer
+}
+_llvm_cov() 
+{
+    local IFS=$' \t\n' cur cur_o prev prev_o prev2 preo
+    local cmd=$1 cmd2 words comp_line2 help args arr i v
+    _llvm_header
+
+    if (( COMP_CWORD == 1 )); then
+        case $cmd in
+            llvm-cov)
+                words=$($cmd --help |& sed -En '/^Subcommands:/,/\a/{ //d; s/:.*$//; p }') ;;
+            llvm-lto2)
+                words=$'dump-symtab\nrun' ;;
+            llvm-pdbutil)
+                words=$'bytes\ndiadump\ndump\nexplain\nexport\nmerge\npdb2yaml
+                pretty\nyaml2pdb' ;;
+            llvm-profdata)
+                words=$'merge\nshow\noverlap' ;;
+        esac
+        COMPREPLY=($(compgen -W "$words" -- "$cur"))
+        return
+    fi
+    cmd2=${COMP_WORDS[1]}
+    help=$({ $cmd $cmd2 --help-list-hidden || $cmd $cmd2 --help-hidden || 
+            $cmd $cmd2 --help-list || $cmd $cmd2 --help ;} 2>&1 )
+
+    if [[ $cur == -*[[*?]* ]]; then
+        _llvm_search
+
+    elif [[ $cur == -* ]]; then
+        words=$(<<< $help sed -En 's/^[ ]{,10}(--?[[:alnum:]][^ =,]*)(=?).*/\1\2/; tX; b; :X s/(.*)\[=/\1\n\1=/; p')
+    
+    elif [[ $prev == -* || "," == @($cur_o|$prev_o) ]]; then
+        [[ $prev == -* ]] && args=$prev || args=$preo
+        words=$(<<< $help sed -En '/'"$args"'/{ :X n; s/^[ ]{,10}=([^ ]+).*/\1/p; tX}')
+    fi
+
+    _llvm_footer
+}
+
+complete -o default -o bashdefault -F _llvm \
+    llc lli opt bugpoint dsymutil \
+    obj2yaml yaml2obj sanstats verify-uselistorder \
+    llvm-addr2line llvm-ar llvm-as llvm-bcanalyzer llvm-c-test llvm-cat llvm-cfi-verify \
+    llvm-config llvm-cxxfilt llvm-dis llvm-dlltool llvm-dwarfdump \
+    llvm-exegesis llvm-extract llvm-link llvm-lto llvm-mc llvm-mca \
+    llvm-modextract llvm-nm llvm-objcopy llvm-objdump llvm-opt-report \
+    llvm-ranlib llvm-readelf llvm-readobj llvm-rtdyld llvm-size llvm-split \
+    llvm-stress llvm-strings llvm-strip llvm-symbolizer llvm-tblgen \
+    llvm-undname llvm-xray clang-format clang-format-diff ld.lld clang-cpp clangd
+complete -o default -o bashdefault -F _llvm_cov llvm-cov llvm-lto2 llvm-pdbutil \
+    llvm-profdata
