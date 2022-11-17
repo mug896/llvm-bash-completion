f333ae7161504665096170c2a3e8f6102296304c
Hyunho Cho <mug896@gmail.com>
2022-11-17 17:33:40 +0900

    update

--- llvm-bash-completion.sh
+++ llvm-bash-completion.sh
@@ -30,7 +30,7 @@ _llvm_header()
 }
 _llvm_footer()
 {
-    if ! declare -p COMPREPLY 2> /dev/null; then
+    if ! declare -p COMPREPLY &> /dev/null; then
         words=$( <<< $words sed -E 's/^[[:blank:]]+|[[:blank:]]+$//g' )
         IFS=$'\n' COMPREPLY=($(compgen -W "$words" -- "$cur"))
     fi
