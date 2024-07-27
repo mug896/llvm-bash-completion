81168f4ea804cd98101cae41132b486ceec20b9e
Hyunho Cho <mug896@gmail.com>
2024-07-27 21:06:01 +0900

    update

--- llvm-bash-completion.sh
+++ llvm-bash-completion.sh
@@ -50,7 +50,7 @@ _llvm_option_list()
 }
 _llvm_search()
 {
-    local res
+    local res IFS=$'\n'
     words=$( _llvm_option_list | sed -E 's/^[ \t]+|[ \t]+$//g' | sort -u )
     for v in $words; do
         if [[ $v == $cur ]]; then
